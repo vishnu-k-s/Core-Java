f74d3800802ba996912f97226aa36580d8a98b4e
vishnu-k-s <58019613+vishnu-k-s@users.noreply.github.com>
2019-12-03 11:30:32 +0530

    Add files via upload

---
+++ overr.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class overr {
+	public static void main(String[] args) {
+		
+		overrb obj=new ch1();
+		obj.show();
+		overrb obj1=new child();
+		obj1.show();
+		overrb obj2=new overrb ();
+		obj2.show();
+	}	
+}
+
