d4ea4013a00bb00d5937b8d99db2b883a527f999
vishnu-k-s <58019613+vishnu-k-s@users.noreply.github.com>
2019-12-12 12:32:13 +0530

    java program for implementing  auto boxing

--- autoboxing.java
+++ autoboxing.java
@@ -1,13 +1,13 @@
+//package name
 package wraper;
 
-public class autoboxing {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Integer i=20;
-		Integer j=i;
+public class autoboxing 
+{
+	public static void main(String[] args) 
+	{
+		Integer i=20;  //AUTO BOXING
+		Integer j=i;   //AUTO BOXING
 		System.out.println(i+" "+j);
-
 	}
 
 }
