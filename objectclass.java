3cfb786599131ba96f0fb9a30b20afc5d2efa691
vishnu-k-s <58019613+vishnu-k-s@users.noreply.github.com>
2019-12-12 12:47:31 +0530

    java program for implementing object class

--- objectclass.java
+++ objectclass.java
@@ -1,29 +1,32 @@
+//package name
 package objectclass;
-public class objtclass {
+
+public class objtclass 
+{
 	static int last_roll=100;
 	int roll_no;
-	objtclass()
-	 {
+	objtclass()     //CONSTRUCTOR METHOD
+	{
 		 roll_no=last_roll;
 		 last_roll++;
-	 }
-public int hashcode()
-{
-	return roll_no;
-}
-public static void main(String args[])
-{
-	objtclass s=new objtclass();
-	System.out.println(s);
-	System.out.println(s.toString());
-	Object obj=new String("FACE");
-	Class c=obj.getClass();
-	System.out.println("Class of object obj is:"+c.getName());
-	s=null;
-	System.gc();
-}
-protected void finalize()
-{
-	System.out.println("finalize method called");
-}
+	}
+	public int hashcode()     // METHOD FOR RETURNING HASHCODE VALUE OF OBJECT
+	{
+	         return roll_no;
+	}
+	public static void main(String args[])
+	{
+		objtclass s=new objtclass();	
+		System.out.println(s);
+		System.out.println(s.toString());
+		Object obj=new String("FACE");	
+		Class c=obj.getClass();
+		System.out.println("Class of object obj is:"+c.getName());
+		s=null;
+		System.gc();      //GARBAGE COLLECTION
+	}
+	protected void finalize()
+	{
+		System.out.println("finalize method called");
+	}
 }
