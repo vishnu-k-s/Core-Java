e5ec418ba6a9787b153ac677c0a7ca599f44c0f7
vishnu-k-s <58019613+vishnu-k-s@users.noreply.github.com>
2019-12-31 09:35:09 +0530

    Add files via upload
    Implementation of Wrapper class using java

---
+++ WrapperClass/autoboxing.java
@@ -0,0 +1,13 @@
+package WrapperClass;
+
+public class autoboxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer i=20;
+		Integer j=i;
+		System.out.println(i+" "+j);
+
+	}
+
+}
---
+++ WrapperClass/unboxing.java
@@ -0,0 +1,14 @@
+package WrapperClass;
+
+public class unboxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer a= new Integer(10);
+		int i=a.intValue();
+	    int j=i;
+	    System.out.println(a+" "+i+" " +j);
+
+	}
+
+}
